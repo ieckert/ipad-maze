7490add072bc666adc611d754cb6f168b1639c6d
Chris Knadler <takeshi91k@gmail.com>
2013-01-27 17:57:37 -0800

    Refactors RootViewController
    A small general refactor on RootViewController. Contains the following:
    
    * Get as close to the 80 char soft limit as possible
    * Unify formatting, particularly with functions
    * Add sanity check assertions where necessary
    * Clean up file headers and add correct license information
    * Add notes on deprecated functions, tagging for future removal.

--- Maze/RootViewController.m
+++ Maze/RootViewController.m
@@ -3,119 +3,80 @@
 //  Maze
 //
 //  Created by ian on 1/21/13.
-//  Copyright __MyCompanyName__ 2013. All rights reserved.
-//
-
-//
-// RootViewController + iAd
-// If you want to support iAd, use this class as the controller of your iAd
+//  Copyright 2013 Ian Eckert. See LICENSE for details.
 //
 
 #import "cocos2d.h"
-
 #import "RootViewController.h"
 #import "GameConfig.h"
 
 @implementation RootViewController
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
- - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-	if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
-	// Custom initialization
-	}
-	return self;
- }
- */
-
-/*
- // Implement loadView to create a view hierarchy programmatically, without using a nib.
- - (void)loadView {
- }
- */
-
-/*
- // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
- - (void)viewDidLoad {
-	[super viewDidLoad];
- }
- */
-
-
 // Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-	
-	//
-	// There are 2 ways to support auto-rotation:
-	//  - The OpenGL / cocos2d way
-	//     - Faster, but doesn't rotate the UIKit objects
-	//  - The ViewController way
-	//    - A bit slower, but the UiKit objects are placed in the right place
-	//
-	
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // There are 2 ways to support auto-rotation:
+    //  - The OpenGL / cocos2d way
+    //     - Faster, but doesn't rotate the UIKit objects
+    //  - The ViewController way
+    //    - A bit slower, but the UiKit objects are placed in the right place
+    
 #if GAME_AUTOROTATION==kGameAutorotationNone
-	//
 	// EAGLView won't be autorotated.
 	// Since this method should return YES in at least 1 orientation, 
 	// we return YES only in the Portrait orientation
-	//
-	return ( interfaceOrientation == UIInterfaceOrientationPortrait );
+    
+	return interfaceOrientation == UIInterfaceOrientationPortrait;
 	
 #elif GAME_AUTOROTATION==kGameAutorotationCCDirector
-	//
 	// EAGLView will be rotated by cocos2d
-	//
+    //
 	// Sample: Autorotate only in landscape mode
-	//
-	if( interfaceOrientation == UIInterfaceOrientationLandscapeLeft ) {
+	if (interfaceOrientation == UIInterfaceOrientationLandscapeLeft)
 		[[CCDirector sharedDirector] setDeviceOrientation: kCCDeviceOrientationLandscapeRight];
-	} else if( interfaceOrientation == UIInterfaceOrientationLandscapeRight) {
+	else if (interfaceOrientation == UIInterfaceOrientationLandscapeRight)
 		[[CCDirector sharedDirector] setDeviceOrientation: kCCDeviceOrientationLandscapeLeft];
-	}
 	
 	// Since this method should return YES in at least 1 orientation, 
 	// we return YES only in the Portrait orientation
-	return ( interfaceOrientation == UIInterfaceOrientationPortrait );
+	return interfaceOrientation == UIInterfaceOrientationPortrait;
 	
 #elif GAME_AUTOROTATION == kGameAutorotationUIViewController
-	//
 	// EAGLView will be rotated by the UIViewController
 	//
 	// Sample: Autorotate only in landscpe mode
 	//
 	// return YES for the supported orientations
 	
-	return ( UIInterfaceOrientationIsLandscape( interfaceOrientation ) );
+	return UIInterfaceOrientationIsLandscape(interfaceOrientation);
 	
 #else
 #error Unknown value in GAME_AUTOROTATION
 	
 #endif // GAME_AUTOROTATION
 	
-	
-	// Shold not happen
-	return NO;
+	// Should not happen
+    assert(false);
+	return false;
 }
 
-//
 // This callback only will be called when GAME_AUTOROTATION == kGameAutorotationUIViewController
-//
 #if GAME_AUTOROTATION == kGameAutorotationUIViewController
--(void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
+                                duration:(NSTimeInterval)duration
 {
-	//
 	// Assuming that the main window has the size of the screen
 	// BUG: This won't work if the EAGLView is not fullscreen
-	///
 	CGRect screenRect = [[UIScreen mainScreen] bounds];
 	CGRect rect = CGRectZero;
-
 	
-	if(toInterfaceOrientation == UIInterfaceOrientationPortrait || toInterfaceOrientation == UIInterfaceOrientationPortraitUpsideDown)		
+	if ((toInterfaceOrientation == UIInterfaceOrientationPortrait) ||
+        (toInterfaceOrientation == UIInterfaceOrientationPortraitUpsideDown))
 		rect = screenRect;
 	
-	else if(toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft || toInterfaceOrientation == UIInterfaceOrientationLandscapeRight)
-		rect.size = CGSizeMake( screenRect.size.height, screenRect.size.width );
+	else if((toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft) ||
+            (toInterfaceOrientation == UIInterfaceOrientationLandscapeRight))
+		rect.size = CGSizeMake(screenRect.size.height, screenRect.size.width);
 	
 	CCDirector *director = [CCDirector sharedDirector];
 	EAGLView *glView = [director openGLView];
@@ -125,29 +86,25 @@
 		rect.size.width *= contentScaleFactor;
 		rect.size.height *= contentScaleFactor;
 	}
+    
 	glView.frame = rect;
 }
 #endif // GAME_AUTOROTATION == kGameAutorotationUIViewController
 
-
-- (void)didReceiveMemoryWarning {
-    // Releases the view if it doesn't have a superview.
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
 }
 
-- (void)viewDidUnload {
+- (void)viewDidUnload
+{
+    // TODO: Remove if unnecessary, currently deprecated
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
-
-- (void)dealloc {
+- (void)dealloc
+{
     [super dealloc];
 }
 
-
 @end
-
